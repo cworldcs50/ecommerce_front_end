34a207a966ae59ff3207b071f3b45594920c43ac
cworldcs50 <114599540+cworldcs50@users.noreply.github.com>
2025-08-31 15:33:16 +0300

    complete test data controller

--- lib/controller/test_data_controller.dart
+++ lib/controller/test_data_controller.dart
@@ -1,8 +1,31 @@
 import 'package:get/get.dart';
+import '../core/constants/enums/request_status.dart';
+import '../core/functions/handling_data.dart';
 import '../core/services/services.dart';
 import '../data/datasource/remote/test_data.dart';
 
 class TestDataController extends GetxController {
   final TestData testData = TestData(api: Get.find<Services>().api);
 
+  RequestStatus requestStatus = RequestStatus.loading;
+  List data = [];
+
+  Future<void> getData() async {
+    final result = await testData.getData();
+    final RequestStatus resultStatus = handlingData(result);
+
+    if (resultStatus == RequestStatus.success) {
+      data.addAll(result["data"]);
+    } else {
+      requestStatus = resultStatus;
+    }
+
+    update();
+  }
+
+  @override
+  void onInit() async {
+    await getData();
+    super.onInit();
+  }
 }
--- lib/data/datasource/remote/test_data.dart
+++ lib/data/datasource/remote/test_data.dart
@@ -1,14 +1,16 @@
+import 'package:dartz/dartz.dart';
 import '../../../core/class/api.dart';
 import '../../../core/constants/app_api_links.dart';
+
 
 class TestData {
   const TestData({required this.api});
 
   final Api api;
 
-  getData() async {
+  dynamic getData() async {
     var response = await api.post(AppApiLinks.kTest, {});
 
-    response.fold((l) {}, (r) {});
+    return response.fold((l) => Left(l), (r) => Right(r));
   }
 }
